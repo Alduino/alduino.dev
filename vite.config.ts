8ebbbe8bd5d580770e2349e9b761db0a9613264e
Alduino <git@alduino.dev>
2024-06-26 21:47:16 +1000

    Fix build
    See https://github.com/vanilla-extract-css/vanilla-extract/issues/1403 for context

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import vike from "vike/plugin";
 import react from "@vitejs/plugin-react-swc";
-import {vanillaExtractPlugin} from "@vanilla-extract/vite-plugin";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [react(), vike(), vanillaExtractPlugin()]
+    plugins: [react(), vike({ prerender: true }), vanillaExtractPlugin()],
 });
